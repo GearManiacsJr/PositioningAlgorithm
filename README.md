4aee9cf0e55871aebc1b5419f4db1a0254403f1d
Alex Sofonea <70516886+Alex05007@users.noreply.github.com>
2024-02-12 19:10:11 +0200

    Importing algorithm

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# PositioningAlgorithm
+## Gear Maniacs Jr Robot Positioning Algorithm
+
+This is the GitHub repository for the Gear Maniacs Jr Robot Positioning Algorithm, a powerful algorithm designed to precisely control the movement and positioning of the Gear Maniacs Jr robot on the competition field.
+
+### Features
+
+* **Precise Movement:** The algorithm utilizes advanced calculations and sensor data to ensure the robot reaches its desired coordinates with unmatched accuracy.
+* **Multiple Movement Options:** Move the robot forward, backward, or rotate to any specified location with the `MoveForward`, `MoveBackward` functions, all tailored for both blue and red courses.
+* **Adjustable Lifting:** Precisely control the robot's arm using the `lift` function, allowing for strategic gear manipulation.
+* **Gyro Integration:** The algorithm seamlessly integrates with the robot's gyro sensor for accurate angle measurements and precise rotations.
+* **Easy to Use:** The provided functions offer a simple and intuitive interface for controlling the robot's movement, making it accessible for both experienced and beginner programmers.
+
+### How it Works
+
+The algorithm leverages a combination of mathematical calculations and sensor data to determine the optimal movement path for the robot. It employs trigonometric functions to calculate the required rotation angles based on the target coordinates and the robot's current position. The gyro sensor provides real-time feedback on the robot's orientation, ensuring accurate execution of the movement commands.
+
+### Getting Started
+
+1. **Clone this repository:** Use `git clone https://github.com/GearManiacsJr/PositioningAlgorithm.git` to clone this repository locally.
+2. **Install dependencies:** Make sure you have all the necessary libraries installed (`math`, `time`, and the libraries for `EV3`).
+3. **Update configuration:** Modify the `x` and `y` variables in the `main.py` file to match your robot's starting position.
+4. **Run the code:** Execute the `main.py` file to test the algorithm.
---
+++ algorithm.py
@@ -0,0 +1,90 @@
+import math
+import time
+from main import gyro, robot
+
+x = 20
+y = 20
+
+def reset():
+    x = 20
+    y = 20
+    gyro.reset_angle(0)
+
+def rad2deg(rad):
+    return rad * 180 / math.pi
+
+def rot(deg):
+    if gyro.angle() < deg:
+        while gyro.angle() < deg:
+            robot.drive(0, 45)
+            #print(str(deg) + " " + str(gyro.angle()))
+        robot.stop()
+    elif gyro.angle() > deg:
+        while gyro.angle() > deg:
+            robot.drive(0, -45)
+            #print(str(deg) + " " + str(gyro.angle()))
+        robot.stop()
+    pass
+
+
+def MoveForwardBlue(x1, y1, rotation1):
+    global x, y
+    alpha = rad2deg(math.atan((y1 - y) / (x1 - x)))
+    if (x1 > x and y1 > y) or (x1 > x and y > y1): 
+        rot(alpha)
+    elif (x > x1 and y > y1) or (y1 > y and x > x1):
+        rot(alpha - 180)
+    d = math.sqrt((x1 - x) * (x1 - x) + (y1 -y) * (y1 - y)) * 10
+    robot.straight(d)
+    rot(rotation1)
+    x = x1
+    y = y1
+    pass
+
+
+def MoveBackwardBlue(x1, y1, rotation1):
+    global x, y
+    alpha = rad2deg(math.atan((y1 - y) / (x1 - x)))
+    if (x1 > x and y1 > y) or (x1 > x and y > y1): 
+        rot((180 - alpha) * -1)
+    elif (x > x1 and y > y1) or (y1 > y and x > x1):
+        rot(alpha)
+    d = math.sqrt((x1 - x) * (x1 - x) + (y1 -y) * (y1 - y)) * 10
+    robot.straight(-d)
+    rot(rotation1)
+    x = x1
+    y = y1
+    pass
+
+def MoveForwardRed(x1, y1, rotation1):
+    global x, y
+    alpha = rad2deg(math.atan((y1 - y) / (x1 - x)))
+    if (x1 > x and y1 > y) or (x1 > x and y > y1): 
+        rot(-alpha)
+    elif (x > x1 and y > y1) or (y1 > y and x > x1):
+        rot((alpha - 180) * -1)
+    d = math.sqrt((x1 - x) * (x1 - x) + (y1 -y) * (y1 - y)) * 10
+    robot.straight(d)
+    rot(rotation1)
+    x = x1
+    y = y1
+    pass
+
+def MoveBackwardRed(x1, y1, rotation1):
+    global x, y
+    alpha = rad2deg(math.atan((y1 - y) / (x1 - x)))
+    if (x1 > x and y1 > y) or (x1 > x and y > y1): 
+        rot((alpha - 180) * -1)
+    elif (x > x1 and y > y1) or (y1 > y and x > x1):
+        rot(-alpha)
+    d = math.sqrt((x1 - x) * (x1 - x) + (y1 -y) * (y1 - y)) * 10
+    robot.straight(-d)
+    rot(rotation1)
+    x = x1
+    y = y1
+    pass
+
+def lift(d):
+    d = d * 360 / (2 * 1.6 * math.pi)
+    brat.run_angle(4000, d)
+    pass
