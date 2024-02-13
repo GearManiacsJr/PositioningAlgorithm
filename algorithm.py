ea52a7f69d219d3631e14721944d26ce5f1ae751
Alex Sofonea <70516886+Alex05007@users.noreply.github.com>
2024-02-13 14:25:27 +0200

    Algorithm short round cases. Everything works now.

--- algorithm.py
+++ algorithm.py
@@ -6,9 +6,11 @@ x = 20
 y = 20
 
 def reset():
+    global x, y
     x = 20
     y = 20
     gyro.reset_angle(0)
+    pass
 
 def rad2deg(rad):
     return rad * 180 / math.pi
@@ -32,8 +34,10 @@ def MoveForwardBlue(x1, y1, rotation1):
     alpha = rad2deg(math.atan((y1 - y) / (x1 - x)))
     if (x1 > x and y1 > y) or (x1 > x and y > y1): 
         rot(alpha)
-    elif (x > x1 and y > y1) or (y1 > y and x > x1):
+    elif (x > x1 and y > y1):
         rot(alpha - 180)
+    elif(y1 > y and x > x1):
+        rot(alpha + 180)
     d = math.sqrt((x1 - x) * (x1 - x) + (y1 -y) * (y1 - y)) * 10
     robot.straight(d)
     rot(rotation1)
@@ -45,7 +49,9 @@ def MoveForwardBlue(x1, y1, rotation1):
 def MoveBackwardBlue(x1, y1, rotation1):
     global x, y
     alpha = rad2deg(math.atan((y1 - y) / (x1 - x)))
-    if (x1 > x and y1 > y) or (x1 > x and y > y1): 
+    if (x1 > x and y1 > y):
+        rot((180 - alpha) * -1)
+    elif (x1 > x and y > y1):
         rot((180 - alpha) * -1)
     elif (x > x1 and y > y1) or (y1 > y and x > x1):
         rot(alpha)
@@ -61,20 +67,25 @@ def MoveForwardRed(x1, y1, rotation1):
     alpha = rad2deg(math.atan((y1 - y) / (x1 - x)))
     if (x1 > x and y1 > y) or (x1 > x and y > y1): 
         rot(-alpha)
-    elif (x > x1 and y > y1) or (y1 > y and x > x1):
+    elif (x > x1 and y > y1):
         rot((alpha - 180) * -1)
+    elif (y1 > y and x > x1):
+        rot(-alpha - 180)
     d = math.sqrt((x1 - x) * (x1 - x) + (y1 -y) * (y1 - y)) * 10
     robot.straight(d)
     rot(rotation1)
     x = x1
     y = y1
     pass
+    
 
 def MoveBackwardRed(x1, y1, rotation1):
     global x, y
     alpha = rad2deg(math.atan((y1 - y) / (x1 - x)))
-    if (x1 > x and y1 > y) or (x1 > x and y > y1): 
+    if (x1 > x and y1 > y): 
         rot((alpha - 180) * -1)
+    elif (x1 > x and y > y1):
+        rot(-alpha - 180)
     elif (x > x1 and y > y1) or (y1 > y and x > x1):
         rot(-alpha)
     d = math.sqrt((x1 - x) * (x1 - x) + (y1 -y) * (y1 - y)) * 10
